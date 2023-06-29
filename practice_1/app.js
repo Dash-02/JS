116e6b149538c03eb6ad331299d784850c6cab47
Dash <dasha.semivolos@gmail.com>
2023-06-29 20:26:38 +0300

    pr1

--- practice_1/app.js
+++ practice_1/app.js
@@ -1,53 +1,12 @@
-/*
-let number = 42
-let myName = 'Dasha'
-const Proger = true
-*/
-// Proger = false // error because of const
-
-/* Can do
-let $ = 'test'
-let $number = 12
-let number$ = 21
-let _ = 34
-let _n = 34
-let n_ = 34
-
-let first_name = 'Joe' // bad
-let firstName = 'John' // good
-*/
-
-//alert(myName)
-//console.log('Your name: ', myName, number)
-
-/*console.log(number + 10)
-console.log(number - 10)
-console.log(number * 10)
-console.log(number / 10)
-console.log(number)*/
-
-/*let number2 = number + 10
-
-console.log(number, number2)
-
-number = number2 - number
-console.log(number, number2)
-
-let number3 = (number + 20)
-console.log(number3)
-*/
-//const fullName = myName + ' Sem'
-//console.log(fullName)
-
-//console.log(result.textContent)
-//result.textContent = 42
-
 const result = document.getElementById('result')
 const input1 = document.getElementById('input1')
 const input2 = document.getElementById('input2')
 const calculate = document.getElementById('submit')
 const calculatePlus = document.getElementById('plus')
 const calculateMinus = document.getElementById('minus')
+const calculateDiv = document.getElementById('division')
+const calculateMult = document.getElementById('multiplication')
+const calculatePow = document.getElementById('pow')
 let action = '+'
 
 //console.log(typeof sum)
@@ -58,15 +17,42 @@ calculatePlus.onclick = function () {
 calculateMinus.onclick = function () {
     action = '-'
 }
+calculateDiv.onclick = function () {
+    action = '/'
+}
+calculateMult.onclick = function () {
+    action = '*'
+}
+calculatePow.onclick = function () {
+    action = '^'
+}
+function printResult(resultElement) {
+    if (resultElement < 0) {
+        result.style.color = 'red'
+    } else {
+        result.style.color = 'green'
+    }
+    result.textContent = resultElement
+}
+
+function computeNumbersWithAction (inp1, inp2, actionSymbol) {
+    const num1 = Number(inp1.value)
+    const num2 = Number(inp2.value)
+    if (actionSymbol == '+') {
+        return num1 + num2
+    } else if (actionSymbol == '-') {
+        return num1 - num2
+    } else if (actionSymbol == '/') {
+        return num1 / num2
+    } else if (actionSymbol == '*') {
+        return num1 * num2
+    } else if (actionSymbol == '^') {
+        return Math.pow(num1, num2)
+    }
+    //return actionSymbol == '+' ? num1 + num2 : num1 - num2
+}
+
 calculate.onclick = function () {
-
-    if (action == '+') {
-        const sum = Number(input1.value) + Number(input2.value)
-        result.textContent = sum
-    }
-
-    if (action == '-') {
-        const difference = Number(input1.value) - Number(input2.value)
-        result.textContent = difference
-    }
+    const resultFunc = computeNumbersWithAction(input1, input2, action)
+    printResult(resultFunc)
 }
