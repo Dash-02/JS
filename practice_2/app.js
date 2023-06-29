9bbed8745aa1080b8b8ffec7527cd32ed9a08406
Dash <dasha.semivolos@gmail.com>
2023-06-29 23:29:07 +0300

    pr2

---
+++ practice_2/app.js
@@ -0,0 +1,9 @@
+// Theory
+
+const array = [1, 2, 3, 4, 5]
+// const arrStr = ['a', 'b', 'd', null, 15]
+// const array1 = new Array (1, 2, 3, 4, 5)
+
+//console.log(array.length)
+console.log(array[0])
+//console.log(array[array.length]) // undefined array[5]
