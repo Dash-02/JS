c5aedf0017eb42aae156cc4f59e89e62d39cacfb
Dash <dasha.semivolos@gmail.com>
2023-07-01 14:24:05 +0300

    pr_2

--- practice_2/app.js
+++ practice_2/app.js
@@ -16,39 +16,32 @@ const inputText = document.getElementById('titleNote')
 const addBtn = document.getElementById('adding')
 const listEl = document.getElementById('list')
 
-const arrNotes = ['note 1', 'note 2']
+//const arrNotes = ['note 1', 'note 2']
+/*
+// function rend () {
 
-function rend () {
+//     for (let i = 0; i < arrNotes.length; i++) {
+//         listEl.insertAdjacentHTML('beforeend', getNoteTemplate(arrNotes[i]))    
+//     }
+// }
 
-    for (let i = 0; i < arrNotes.length; i++) {
-        listEl.insertAdjacentHTML('beforeend', getNoteTemplate(arrNotes[i]))    
-    }
-}
-
-rend()
+// rend()
 
 addBtn.onclick = function () {
 
     if (inputText.value.length === 0) {
         return
     }
+
+    // insertAdjacentHTML() разбирает указанный текст как HTML или XML и 
+    // вставляет полученные узлы (nodes) в DOM дерево в указанную позицию
+    // 'beforeend': сразу перед закрывающим тегом element (после последнего потомка)
+
     listEl.insertAdjacentHTML('beforeend', 
-        getNoteTemplate(inputText.value)
-    )
+        getNoteTemplate(inputText.value))
     inputText.value = ''
 }
-
-function getNoteTemplate(title) {
-    return `
-    <li>
-        <span>${title}</span>
-        <span>
-            <span>&check;</span>
-            <span>&times;</span>
-        </span>
-    </li>
-`
-}
+*/
 
 /* Object Theory
 
@@ -68,13 +61,73 @@ const key = 'hasGirlfriend'
 console.log(human[key])
 */
 
-const notes = [
+// массив из объектов
+const arrNotes = [
     {
         title: 'note_1',
         completed: false,
     },
     {
         title: 'note_2',
-        completed: false,
+        completed: true,
     },
 ]
+
+function rend () {
+    listEl.innerHTML = ""
+    if (arrNotes.length === 0) {
+        listEl.innerHTML = '<p>No notes</p>'
+    }
+    for (let i = 0; i < arrNotes.length; i++) {
+        listEl.insertAdjacentHTML('beforeend', getNoteTemplate(arrNotes[i], i))    
+    }
+}
+
+rend()
+
+addBtn.onclick = function () {
+    if (inputText.value.length === 0) {
+        return
+    }
+    const newText = {
+        title: inputText.value,
+        completed: false,
+    }
+    arrNotes.push(newText)
+    rend()
+    inputText.value = ''
+}
+
+listEl.onclick = function (event) {
+    //console.log(event.target.dataset.index)
+    if (event.target.dataset.index) {
+        const index = parseInt(event.target.dataset.index)
+        const type = event.target.dataset.type
+
+        if (type === 'target') {
+            arrNotes[index].completed = !arrNotes[index].completed
+        } else if (type === 'remove') {
+            arrNotes.splice(index, 1)
+        }
+        rend()
+    }
+}
+
+function getNoteTemplate(arrNotes, index) {
+    return `
+    <li 
+        class="list-group-item d-flex justify-content-between 
+        align-items-center"
+    >
+        <span class ="${arrNotes.completed ? 'text-decoration-line-through' : ''}">${arrNotes.title}</span>
+        <span>
+            <span class="btn btn-small btn-${arrNotes.completed ? 'warning' : 'success'}" data-index="${index}" data-type="target">
+                &check;
+            </span>
+            <span class="btn btn-small btn-danger" data-index="${index}" data-type="remove">
+                &times;
+            </span>
+        </span>
+    </li>
+`
+}
