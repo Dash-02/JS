1b99badf00c0db2a05d8d18a0ee0659c235cd78d
Dash <dasha.semivolos@gmail.com>
2023-07-06 16:17:18 +0300

    quickSort

--- algorithms/quick_sort.js
+++ algorithms/quick_sort.js
@@ -1,29 +1,29 @@
 const arr = [4, 2, 45, 21, 67, 3, 6, 34, 8, 10, 53]
-const arrMin = []
-const arrMax = []
 let count = 0
 
 function quickSort (arr) {
-    const lastEl = arr[arr.length-1]
-    /*for (let i = 0; i < arr.length; i++) {
-      if (lastEl < arr[i]) {
-        arrMax[i] = arr[i]
-      } else {
-        arrMin[i] = arr[i]
-      }
-    }*/
+    
+    let pivotInd = arr.length-1
+    let pivot = arr[arr.length-1]
+    let min = []
+    let max = []
+
+    if (arr.length <= 1) {
+        return arr
+    }
 
     for (let i = 0; i < arr.length; i++) {
         count++
-        for (let j = 0; j < arr.length; j++) {
-           if (lastEl < arr[j]) {
-                let temp = arr[i]
-                arr[i] = arr[j]
-                arr[j] = temp
-           }
+        if (i === pivotInd) {
+            continue
+        }
+        if (arr[i] < pivot) {
+            min.push(arr[i])
+        } else {
+            max.push(arr[i])
         }
     }
-    return arr;
+    return [...quickSort(min), pivot, ...quickSort(max)];
 }
 
 console.log(quickSort(arr))
