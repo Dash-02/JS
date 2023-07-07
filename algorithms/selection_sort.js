08a3cf550d0306df8694fbd96b31450db04c8f33
Dash <dasha.semivolos@gmail.com>
2023-07-07 17:28:08 +0300

    selectionSort2

--- algorithms/selection_sort.js
+++ algorithms/selection_sort.js
@@ -1,18 +1,35 @@
 const arr = [-2, 4, -3, 7, 0, 12, 5, 3, 9, 6]
+let count = 0
 
-function selectionSort(arr) {
+function selectionSort1(arr) {
     for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length; j++) {
-           if (arr[i] < arr[j]) {
-                let min = arr[i]
-                arr[i] = arr[j]
-                arr[j] = min
+        let min = i
+        for (let j = i + 1; j < arr.length; j++) {
+           if (arr[j] < arr[min]) {
+                min = j
            }
-            
+           count++
         }
-        
+        let tmp = arr[i]
+        arr[i] = arr[min]
+        arr[min] = tmp
     }
     return arr;
 }
 
-console.log(selectionSort(arr))
+function selectionSort2(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+           if (arr[j] < arr[i]) { 
+                let tmp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = tmp
+           }
+           count++
+        }
+    }
+    return arr;
+}
+
+console.log(selectionSort2(arr))
+console.log(count)
