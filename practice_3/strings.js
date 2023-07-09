913f71d709b5080bebfe65c1eadfc9c35528fcdf
Dash <dasha.semivolos@gmail.com>
2023-07-09 20:52:47 +0300

    theory strings

---
+++ practice_3/strings.js
@@ -0,0 +1,27 @@
+const name1 = 'Dash'
+const age = 19
+// const oldVar = 'Hello, ' + name 
+
+function myName() {
+    return name1
+}
+
+const output1 = `Hello, ${myName()}`
+const output2 = `Hello, I\'m ${age} years old and I ${age > 18 ? 'can drive' : 'can\'t drive'}`
+
+const output = `any 
+string`
+
+// console.log(output2)
+/*
+console.log(name1.length)
+console.log(name1.toUpperCase())
+console.log(name1.charAt(2)) // показывает какой символ на указанной позиции
+console.log(name1.indexOf('a'))
+console.log(name1.toLowerCase().startsWith('d'))
+console.log(name1.endsWith('s'))
+console.log(name1.repeat(2)) // дублирует строку
+*/
+const password = ' gH jr e g  '
+console.log(password)
+console.log(password.trim()) // убирает пробелы до и после строки
