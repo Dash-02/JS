88ea4e18c5c3dd2d6b4abdce2ceb5099fa105e95
Dash <dasha.semivolos@gmail.com>
2023-07-12 18:48:22 +0300

    object_theory2

--- theory/objects.js
+++ theory/objects.js
@@ -62,7 +62,73 @@ const {age, firstName: name1 = 'Robert', languages} = people // good
 
 // когда массив используется for of
 // когда объект используется for in
-for (let key in people) {
-    console.log(key) // keys of object
-    console.log(people[key]) // values of keys
+// цикл проходит и по прототипам, поэтому необходима проверка
+// для снижения ошибок
+/*for (let key in people) {
+    if (people.hasOwnProperty(key)) {
+        console.log(key) // keys of object
+        console.log(people[key]) // values of keys
+    }
+}*/
+
+// Object.keys(people).forEach((key) => {
+    // console.log(people[key])
+// })
+
+const logger1 = {
+    keys() {
+        console.log('Object keys:', Object.keys(this))
+    },
+
+    keysAndValues() {
+        Object.keys(this).forEach((key) => {
+            console.log('key:', key)
+            console.log('value:', this[key])
+        })
+    },
 }
+// параметр - это контекст (объект)
+// logger1.keys.bind(people)
+// const bound = logger1.keys.bind({ a: 1, b: 3 })
+// bound()
+
+// const bound = logger1.keys.bind({ a: 1, b: 3 })()
+
+// сразу вызывают ф-цию
+// logger2.keys.call(people, false, , , )
+
+// если у ф-ции есть параметры, то в call они все перечисляются
+// в apply ф-ция принимает 2 параметра, 2-й всегда массив
+// logger2.keys.apply(people, [])
+
+
+// =========== Classes
+
+// классы позволяют создавть объекты
+class Human {
+    // статичные поля относятся к самому классу, но не к экземплярам
+   static isHuman = true
+}
+
+class Person extends Human {
+    constructor(name2, age1) {
+        super() // вызывает родительский констуктор
+        this.name2 = name2 ?? 'Undefined name'
+        this.age1 = age1 ?? 'Undefined age'
+    }
+
+    sayHi() {
+        console.log('Hi,', this.name2)
+    }
+}
+
+const person1 = new Person('Sunny', 24)
+const person2 = new Person('Lisa', 25)
+
+// экземпляры класса Person
+// person1.sayHi()
+// person2.sayHi()
+
+console.log(person1.isHuman) // undefined, тк это экземпляр класса
+console.log(Person.isHuman) // true
+console.log(Human.isHuman) // true
