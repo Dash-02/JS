928947f1919b2e9c27db54f0e47cec06be58ec8c
Dash <dasha.semivolos@gmail.com>
2023-07-17 21:47:51 +0300

    pr_4

--- practice_4/app.js
+++ practice_4/app.js
@@ -10,17 +10,45 @@
 // console.log(parsed)
 
 const list = document.querySelector('#list')
+const filt = document.querySelector('#filter')
+let USERS = []
+
+filt.addEventListener('input', (event) => {
+    const value = event.target.value.toLowerCase()
+    //const { value } = event.target
+    const filtUsers = USERS.filter((user) => 
+        user.name.toLowerCase().includes(value)
+    )
+    rend(filtUsers)
+})
+
 async function start() {
+    list.innerHTML = 'Loading...'
     try {
         const resp = await fetch('https://jsonplaceholder.typicode.com/users') // возвращает promise
         const data = await resp.json()
-        console.log(data)
+        setTimeout(() => {
+            USERS = data
+            rend(data)
+        }, 2000)
     } catch (error) {
-        
+        list.style.color = 'red'
+        list.innerHTML = error.message
     }
 }
-start()
 
-function rend() {
-
+function rend(users = []) {
+    if (users.length === 0) {
+        list.innerHTML = 'No matched users('
+    } else {
+        const html = users.map(toHTML).join('')
+        list.innerHTML = html
+    }
 }
+
+function toHTML(user) {
+    return `
+    <li class="list-group-item">${user.name}</li>
+    `
+}
+start()
