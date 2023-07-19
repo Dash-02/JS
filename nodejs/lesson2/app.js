d2f912492800689aa4d25c1c60cfb402c4d0d378
Dash <dasha.semivolos@gmail.com>
2023-07-19 23:41:36 +0300

    lesson2

--- nodejs/lesson2/app.js
+++ nodejs/lesson2/app.js
@@ -6,11 +6,29 @@ fs.readFile('./text.txt', 'utf8', (error, data) => {
     // console.log(data.toString())
     // console.log(data)
 
-     fs.writeFile('./files/text3.txt', `${data} It's new file!`, (error) => {
+    fs.mkdir('./files', () => {
+      fs.writeFile('./files/text2.txt', `${data} It's new file!`, (error) => {
         error ? console.log(error) : null
      })
+   })
+
+   // OR
+/* использовать синхронные методы для соблюдения порядкаа выполнения
+   fs.mkdirSync('./files', () => {})
+   fs.writeFileSync('./files/text2.txt', `${data} It's new file!`, (error) => {
+      error ? console.log(error) : null
+   })
+*/
 })
 
-fs.mkdir('./files', () => {
-    
-})
+setTimeout(() => {
+   if (fs.existsSync('./files/text2.txt')){
+      fs.unlink('./files/text2.txt', () => {})
+   }
+}, 4000)
+
+setTimeout(() => {
+   if (fs.existsSync('./files')){
+      fs.rmdir('./files', () => {})
+   }
+}, 6000)
