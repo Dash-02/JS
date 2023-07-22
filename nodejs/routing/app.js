9a6bb1273a03d6944f7861bdc0a3b81aca88100f
Dash <dasha.semivolos@gmail.com>
2023-07-22 20:19:18 +0300

    22/07

--- nodejs/routing/app.js
+++ nodejs/routing/app.js
@@ -1,15 +1,43 @@
 const http = require('http')
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs') // нужен для работы с файлами, кот. будут возвращать в браузер
+const path = require('path') // чтобы формировать корректный путь
 
 const PORT = 3000
 
 const server = http.createServer((req, res) => {
     console.log('Server request')
   
-    res.setHeader('content-Type', 'text/html')
+    res.setHeader('Content-Type', 'text/html')
 
-    if (req.url = '/') {}
+    const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`)
+
+    let basePath =''
+
+    switch (req.url) {
+        case '/':
+            basePath = createPath('index')
+            break;
+
+        case '/contacts':
+            basePath = createPath('contacts')
+            break;
+    
+        default:
+            basePath = createPath('contacts')
+            break;
+    }
+
+    // проверка адреса, приходящего запроса
+    
+    fs.readFile('./views/index.html', (err, data) => {
+        if (err) {
+            console.log(err)
+            res.end()
+         } else {
+            res.write(data)
+            res.end()
+        }
+    })
 })
 
 server.listen(PORT, 'localhost', (error) => {
