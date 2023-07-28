2600baf6ac824507ca50aae6ed55bff6ec0a2df1
Dash <dasha.semivolos@gmail.com>
2023-07-28 20:36:04 +0300

    express-js

---
+++ nodejs/routing/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+const PORT = 3000
+
+const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`)
+
+app.listen(PORT, 'localhost', (error) => {
+    error ? console.log(error) : console.log(`listening port ${PORT}`)
+})
+
+app.get('/', (req, res) => {
+    // вместо res.write и res.end - res.send
+    // res.send('<h1>hi</h1>') // передаются данные, которые нужно отправить браузеру
+    res.sendFile(createPath('index'))
+})
+
+app.get('/contacts', (req, res) => {
+    res.sendFile(createPath('contacts'))
+})
+
+app.get('/about_us', (req, res) => {
+    res.redirect('contacts')
+})
+
+app.use((req, res) => {
+    // res.statusCode = 404
+    res
+        .status(404)
+        .sendFile('error')
+})
