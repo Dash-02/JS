8038cf60037fe754a2e4a7144b5a478e76e61c20
Dash <dasha.semivolos@gmail.com>
2023-07-31 23:03:49 +0300

    middleware

--- nodejs/routing/server.js
+++ nodejs/routing/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const morgan = require('morgan')
 
 const app = express()
 
@@ -14,16 +15,30 @@ app.listen(PORT, 'localhost', (error) => {
     error ? console.log(error) : console.log(`listening port ${PORT}`)
 })
 
+// app.use((req, res, next) => {
+//     console.log(`path: ${req.path}`)
+//     console.log(`method: ${req.method}`)
+//     next()
+// })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+
+app.use(express.static('styles'))
+
 app.get('/', (req, res) => {
+    const title = 'Home'
     // вместо res.write и res.end - res.send
     // res.send('<h1>hi</h1>') // передаются данные, которые нужно отправить браузеру
 
     // res.sendFile(createPath('index'))
-    res.render(createPath('index'))
+    res.render(createPath('index'), { title })
 })
 
 app.get('/contacts', (req, res) => {
-    res.render(createPath('contacts'))
+    const title = 'Contacts'
+    const contacts = [
+        { name: 'Github', link: 'https://github.com/Dash-02' },
+    ]
+    res.render(createPath('contacts'), { contacts, title })
 })
 
 app.get('/about_us', (req, res) => {
@@ -31,20 +46,24 @@ app.get('/about_us', (req, res) => {
 })
 
 app.get('/posts/:id', (req, res) => {
-    res.render(createPath('post'))
+    const title = 'Post'
+    res.render(createPath('post'), { title })
 })
 
 app.get('/add-post', (req, res) => {
-    res.render(createPath('add-post'))
+    const title = 'Add post'
+    res.render(createPath('add-post'), { title })
 })
 
 app.get('/posts', (req, res) => {
-    res.render(createPath('posts'))
+    const title = 'Posts'
+    res.render(createPath('posts'), { title })
 })
 
 app.use((req, res) => {
+    const title = 'Error Page'
     // res.statusCode = 404
     res
         .status(404)
-        .render('error')
+        .render(createPath('error'), { title })
 })
