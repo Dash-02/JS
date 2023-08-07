f005a88d503a31c46d09fe033173c3db89df269f
Dash <dasha.semivolos@gmail.com>
2023-08-07 18:01:20 +0300

    delete-update

--- nodejs/routing/server.js
+++ nodejs/routing/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
+const methodOverride = require('method-override')
 const Post = require('./models/post')
 const Contact = require('./models/contact')
 
@@ -34,6 +35,8 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 app.use(express.urlencoded({ extended: false }))
 
 app.use(express.static('styles'))
+
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
     const title = 'Home'
@@ -82,6 +85,40 @@ app.get('/posts/:id', (req, res) => {
         })
 })
 
+app.delete('/posts/:id', (req, res) => {
+    const title = 'Post'
+    Post
+        .findByIdAndDelete(req.params.id)
+        .then(result => { res.sendStatus(200) })
+        .catch((error) => {
+            console.log(error)
+            res.render(createPath('error', { title: 'Error' }))
+        })
+})
+
+app.get('/edit/:id', (req, res) => {
+    const title = 'Edit Post'
+    Post
+        .findById(req.params.id)
+        .then((post) => res.render(createPath('edit-post'), { post, title }))
+        .catch((error) => {
+            console.log(error)
+            res.render(createPath('error', { title: 'Error' }))
+        })
+})
+
+app.put('/edit/:id', (req, res) => {
+    const { title, author, text } = req.body
+    const { id } = req.params
+    Post
+        .findByIdAndUpdate(id, { title, author, text })
+        .then(result => res.redirect(`/posts/${id}`))
+        .catch((error) => {
+            console.log(error)
+            res.render(createPath('error', { title: 'Error' }))
+        })
+})
+
 app.post('/add-post', (req, res) => {
     const { title, author, text } = req.body
     const post = new Post({ title, author, text })
@@ -113,6 +150,7 @@ app.get('/posts', (req, res) => {
     // res.render(createPath('posts'), { title, posts })
     Post
         .find()
+        .sort({ createdAt: -1 })
         .then((posts) => res.render(createPath('posts'), { posts, title }))
         .catch((error) => {
             console.log(error)
