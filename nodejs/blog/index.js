896c944449d2b45020020eb10bcdd5bbbedab9b7
Dash <dasha.semivolos@gmail.com>
2023-08-20 22:24:34 +0300

    jwt

--- nodejs/blog/index.js
+++ nodejs/blog/index.js
@@ -1,9 +1,29 @@
 import express from "express"
+import jwt from "jsonwebtoken"
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
-    res.send('Hello')
+    res.send('Hello!')
+})
+
+app.post('/login', (req, res) => {
+    console.log(req.body)
+
+    const token = jwt.sign(
+        {
+            email: req.body.email,
+            fullName: 'JoJo',
+        },
+        'mystery1',
+    )
+
+    res.json({
+        success: true,
+        token,
+    })
 })
 
 app.listen(4444, (err) => {
