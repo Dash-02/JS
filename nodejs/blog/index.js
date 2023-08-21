770a50b79e74d703a6b293f2f815cc8bd92226ba
Dash <dasha.semivolos@gmail.com>
2023-08-21 22:49:02 +0300

    create-user_bcrypt

--- nodejs/blog/index.js
+++ nodejs/blog/index.js
@@ -1,9 +1,12 @@
 import express from "express"
-import jwt from "jsonwebtoken"
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 import mongoose from 'mongoose'
 import { validationResult } from 'express-validator'
 
 import { registerValidation } from './validations/auth.js'
+
+import UserModel from './models/user.js'
 
 mongoose
     .connect('mongodb+srv://user-blog:user0987@cluster0.eljscjc.mongodb.net/?retryWrites=true&w=majority')
@@ -14,15 +17,26 @@ const app = express()
 
 app.use(express.json())
 
-app.post('/auth/register', registerValidation, (req, res) => {
+app.post('/auth/register', registerValidation, async (req, res) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
         return res.status(400).json(errors.array())
     }
 
-    res.json({
-        success: true,
+    const password = req.body.password
+    const salt = await bcrypt.genSalt(10)
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    const doc = new UserModel({
+        email: req.body.email,
+        fullName: req.body.fullName,
+        avatarUrl: req.body.avatarUrl,
+        passwordHash,
     })
+
+    const user = await doc.save()
+
+    res.json(user)
 })
 
 app.listen(4444, (err) => {
