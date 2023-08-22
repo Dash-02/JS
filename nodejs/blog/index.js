55f9f1d6ddb7b9e6e0eed9baaa70348f75198e59
Dash <dasha.semivolos@gmail.com>
2023-08-22 23:38:34 +0300

    auth

--- nodejs/blog/index.js
+++ nodejs/blog/index.js
@@ -9,7 +9,7 @@ import { registerValidation } from './validations/auth.js'
 import UserModel from './models/user.js'
 
 mongoose
-    .connect('mongodb+srv://user-blog:user0987@cluster0.eljscjc.mongodb.net/?retryWrites=true&w=majority')
+    .connect('mongodb+srv://user-blog:user0987@cluster0.eljscjc.mongodb.net/blog?retryWrites=true&w=majority')
     .then(() => console.log('DB connected'))
     .catch((err) => console.log('DB error', err))
 
@@ -17,26 +17,90 @@ const app = express()
 
 app.use(express.json())
 
-app.post('/auth/register', registerValidation, async (req, res) => {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-        return res.status(400).json(errors.array())
+app.post('/auth/login', async (req, res) => {
+    try {
+        const user = await UserModel.findOne({ email: req.body.email })
+
+        if (!user) {
+            return req.status(404).json({
+                message: 'User not found'
+            })
+        }
+
+        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash)
+
+        if (!isValidPass) {
+            return req.status(404).json({
+                message: 'Wrong login or password'
+            })
+        }
+
+        const token = jwt.sign(
+            {
+                id: user._id,
+            }, 
+            'secret1',
+            {
+                expiresIn: '30d'
+            },
+        )
+
+        const { passwordHash, ...userData } = user._doc
+
+        res.json({
+            ...userData,
+            token,
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Failed to login',
+        })
     }
+})
 
-    const password = req.body.password
-    const salt = await bcrypt.genSalt(10)
-    const passwordHash = await bcrypt.hash(password, salt)
+app.post('/auth/register', registerValidation, async (req, res) => {
+    try {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors.array())
+        }
 
-    const doc = new UserModel({
-        email: req.body.email,
-        fullName: req.body.fullName,
-        avatarUrl: req.body.avatarUrl,
-        passwordHash,
-    })
+        const password = req.body.password
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(password, salt)
 
-    const user = await doc.save()
+        const doc = new UserModel({
+            email: req.body.email,
+            fullName: req.body.fullName,
+            avatarUrl: req.body.avatarUrl,
+            passwordHash: hash,
+        })
 
-    res.json(user)
+        const user = await doc.save()
+
+        const token = jwt.sign(
+            {
+                id: user._id,
+            }, 
+            'secret1',
+            {
+                expiresIn: '30d'
+            },
+        )
+
+        const { passwordHash, ...userData } = user._doc
+
+        res.json({
+            ...userData,
+            token,
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Failed to register',
+        })
+    }
 })
 
 app.listen(4444, (err) => {
