4c6ab82755500f9cceabe1406494688fd28c5b74
Dash <dasha.semivolos@gmail.com>
2023-08-24 22:03:47 +0300

    validation-for-create-post

---
+++ nodejs/blog/validations/validations.js
@@ -0,0 +1,24 @@
+import { body } from "express-validator";
+
+export const loginValidation = [
+    body("email", "Invalid email format").isEmail(),
+    body("password", "Password must contain at least 5 characters").isLength({
+        min: 5,
+    }),
+];
+
+export const registerValidation = [
+    body("email", "Invalid email format").isEmail(),
+    body("password", "Password must contain at least 5 characters").isLength({
+        min: 5,
+    }),
+    body("fullName", "Enter a name").isLength({ min: 3 }),
+    body("avatarUrl", "Invalid avatar link").optional().isURL(),
+];
+
+export const postCreateValidation = [
+    body("title", "Введите заголовок статьи").isLength({ min: 3 }).isString(),
+    body("text", "Введите текст статьи").isLength({ min: 3 }).isString(),
+    body("tags", "Неверный формат тэгов").optional().isArray(),
+    body("imageUrl", "Неверная ссылка на изображение").optional().isString(),
+];
