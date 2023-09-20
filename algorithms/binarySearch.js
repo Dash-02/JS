2a2ffb8a8e944028b9fcb5eeb3e979e40d742aa0
Dash <dasha.semivolos@gmail.com>
2023-09-20 22:42:30 +0300

    2

--- algorithms/binarySearch.js
+++ algorithms/binarySearch.js
@@ -2,14 +2,19 @@ const array = [2, 4, 7, 12, 23, 45]
 let count = 0
 let mid = array.length / 2
 let lim = array.length - 1
+let min = array[0]
 
 let num = 45
 
 function binarySearch(arr, n) {
-    for (let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < lim; i++) {
         count++
         if (arr[mid] > n){
             arr[lim] = arr[mid]
+            mid /= 2
+        }
+        if (arr[mid] < n){
+            arr[min] = arr[mid]
             mid /= 2
         }
         return arr[mid];
